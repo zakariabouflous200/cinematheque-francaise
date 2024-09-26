c6146969867be769e47add3df852d4764bd5069a
Bouflous Zakariae <148075492+zakariabouflous200@users.noreply.github.com>
2024-09-26 21:04:25 +0200

    Update Login.js

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -15,7 +15,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/api/users/login', {
+      const response = await fetch('https://cinematheque-francaise.up.railway.app/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
