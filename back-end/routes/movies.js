cbdcf7a497dea998e30a75118d2af1def9afe5fb
Bouflous Zakariae <148075492+zakariabouflous200@users.noreply.github.com>
2024-09-27 19:30:56 +0200

    Update movies.js

--- back-end/routes/movies.js
+++ back-end/routes/movies.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovies, details, moviesInCinemas, getMoviesAtFestivals, searchMovies, getMovieById  } = require('../controllers/moviesController'); 
-router.get('/getMovie/:id', getMovieById);
+const { getAllMovies, details, moviesInCinemas, getMoviesAtFestivals, searchMovies, getMovieByTitle  } = require('../controllers/moviesController'); 
+// Route to search movie by title
+router.get('/getMovieByTitle/:title', getMovieByTitle);
 // Route pour obtenir les détails d'un film
 router.get('/details', details);
 // Route to search movies by title
